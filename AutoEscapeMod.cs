287ba1f6f3c7b983b5b2574bc826bbe60909711f
Ozdeniz Dolu <ozdeniz578@gmail.com>
2025-07-25 14:20:47 +0300

    Add hotkey support for toggle

--- AutoEscapeMod.cs
+++ AutoEscapeMod.cs
@@ -1,21 +1,56 @@
 ﻿using Il2CppAssets.Scripts.Models.Bloons.Behaviors;
 using Il2CppAssets.Scripts.Simulation.Bloons;
 using Il2CppAssets.Scripts.Unity.UI_New.InGame;
+using Il2CppAssets.Scripts;
+using Il2CppAssets.Scripts.Unity.UI_New.Upgrade;
+using Il2CppAssets.Scripts.Unity;
+using Il2CppAssets.Scripts.Unity.UI_New.Popups;
 using AutoEscape;
 using BTD_Mod_Helper;
+using BTD_Mod_Helper.Api.ModOptions;
+using BTD_Mod_Helper.Api.Helpers;
 using BTD_Mod_Helper.Extensions;
 using MelonLoader;
+using UnityEngine;
+using UnityEngine.InputSystem.Utilities;
 
 [assembly: MelonInfo(typeof(AutoEscapeMod), ModHelperData.Name, ModHelperData.Version, ModHelperData.RepoOwner)]
 [assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
 namespace AutoEscape;
 
 public class AutoEscapeMod : BloonsTD6Mod
-{
-    public override bool PreBloonLeaked(Bloon bloon)
+{   
+    private static bool isEnabled = true;
+    private static readonly ModSettingHotkey Hotkey = new(KeyCode.F6)
     {
-        if (bloon.GetModifiedTotalLeakDamage() >= InGame.instance.bridge.GetHealth() + InGame.Bridge.simulation.Shield
-            && !InGameData.CurrentGame.IsSandbox && !bloon.bloonModel.HasBehavior<GoldenBloonModel>())
+        displayName = "Toggle this mod with hotkey:"
+    };
+    public override void OnUpdate()
+    {
+        if (Hotkey.JustPressed())
+        {
+            ToggleMod();
+        }
+    }
+
+    private static void ToggleMod() {
+        isEnabled = !isEnabled;
+        var message = $"AutoEscape is now {(isEnabled ? "enabled!" : "disabled!")}";
+
+        if (InGame.instance == null || InGame.Bridge == null)
+        {
+            ModHelper.Msg<AutoEscapeMod>(message);
+        }
+        else
+        {
+            Game.instance.ShowMessage(message, 1f);
+        }
+    }
+    public override bool PreBloonLeaked(Bloon bloon)
+    {   
+        if (isEnabled 
+        && bloon.GetModifiedTotalLeakDamage() >= InGame.instance.bridge.GetHealth() + InGame.Bridge.simulation.Shield
+        && !InGameData.CurrentGame.IsSandbox && !bloon.bloonModel.HasBehavior<GoldenBloonModel>())
         {
             if (!InGame.instance.quitting)
             {
